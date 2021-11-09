68e8cd52ce5ced688f83d59886feeb92b58c4ba1
Peter Akaliro <pin4eva@hotmail.com>
2021-11-09 10:12:02 +0100

    added docker

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,20 +1,44 @@
-const config = require("../utils/config");
-
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "mongoose",
-      settings: {
-        uri: config.MONGO_URI || env("DATABASE_URI"),
-        srv: env.bool("DATABASE_SRV", true),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "brighter_dawn_cms"),
+module.exports = ({ env }) => {
+  const docker = env("DOCKER");
+  if (docker) {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "mongoose",
+          settings: {
+            host: env("DATABASE_HOST", "bdmis_mongo"),
+            srv: env.bool("DATABASE_SRV", false),
+            port: env.int("DATABASE_PORT", 27017),
+            database: env("DATABASE_NAME", "brighter_dawn_cms"),
+            username: env("DATABASE_USERNAME", "peter"),
+            password: env("DATABASE_PASSWORD", "ifeanyi123"),
+          },
+          options: {
+            authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+            ssl: env.bool("DATABASE_SSL", false),
+          },
+        },
       },
-      options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", true),
+    };
+  } else {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "mongoose",
+          settings: {
+            uri: env("DATABASE_URI", "mongodb://localhost/brigher_dawn_cms"),
+            srv: env.bool("DATABASE_SRV", true),
+            port: env.int("DATABASE_PORT", 27017),
+            database: env("DATABASE_NAME", "brighter_dawn_cms"),
+          },
+          options: {
+            authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+            ssl: env.bool("DATABASE_SSL", true),
+          },
+        },
       },
-    },
-  },
-});
+    };
+  }
+};
