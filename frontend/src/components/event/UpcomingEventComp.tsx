4be6787c9ae49dd8eb4b80a494e69aa792c803d1
Peter Akaliro <pin4eva@gmail.com>
2021-11-26 11:00:25 +0100

    flexed frontLayout

--- frontend/src/components/event/UpcomingEventComp.tsx
+++ frontend/src/components/event/UpcomingEventComp.tsx
@@ -30,26 +30,30 @@ const Wrapper = styled.div`
 
 const SingleEventItem = ({ event }: { event: IEvent }) => (
 	<Wrapper className="eventpage">
-		<div className="eventcal d-flex flex-column flex-lg-row mb-3">
-			<div className="left">
-				<img src={event?.image.url} className=" img-fluid" alt="" />
-			</div>
-			<div className="right p-4">
-				<div className=" d-flex justify-content-between eventcal-top">
-					<p className="fs-12 left">
-						<span className=" me-2">
-							<FontAwesomeIcon icon={faCalendarAlt} width={13} />
-						</span>
-						{dayjs(event?.createdAt).format("YYYY MMM D")}
-					</p>
+		<Link href={`/events/${event?.slug}`}>
+			<a className="link-dark text-decoration-none">
+				<div className="eventcal d-flex flex-column flex-lg-row mb-3">
+					<div className="left">
+						<img src={event?.image.url} className=" img-fluid" alt="" />
+					</div>
+					<div className="right p-4">
+						<div className=" d-flex justify-content-between eventcal-top">
+							<p className="fs-12 left">
+								<span className=" me-2">
+									<FontAwesomeIcon icon={faCalendarAlt} width={13} />
+								</span>
+								{dayjs(event?.createdAt).format("YYYY MMM D")}
+							</p>
+						</div>
+						<Link href={`/events/${event?.slug}`}>
+							<a className=" text-decoration-none" style={{ color: "#000" }}>
+								<p className="josefin fs-24 fw-700">{event?.title}</p>
+							</a>
+						</Link>
+						<p className="fs-14">{event?.description}...</p>
+					</div>
 				</div>
-				<Link href={`/events/${event?.slug}`}>
-					<a className=" text-decoration-none" style={{ color: "#000" }}>
-						<p className="josefin fs-24 fw-700">{event?.title}</p>
-					</a>
-				</Link>
-				<p className="fs-14">{event?.description}...</p>
-			</div>
-		</div>
+			</a>
+		</Link>
 	</Wrapper>
 );
